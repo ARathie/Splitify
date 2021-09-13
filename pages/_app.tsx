7674ed9dec0a434422e3d919d26c945830d1d76e
Ashwin Rathie <Ash@Anitas-MacBook-Pro.local>
2021-09-13 04:46:07 -0700

    redux first pass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
 import { Windmill } from "@windmill/react-ui";
+import React from "react";
 import type { AppProps } from "next/app";
 import "../styles/index.css";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import allReducers from "../reducers";
+
+const store = createStore(allReducers);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Windmill>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     </Windmill>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,79 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Windmill } from "@windmill/react-ui";
-
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import { Button } from "@windmill/react-ui";
+import MyApp from "./_app";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import allReducers from "../reducers";
 
-const element = <h1>Hello World</h1>;
+const store = createStore(allReducers);
 
-const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Splitify</title>
-        <meta
-          name="description"
-          content="quickly settle debts with your friends"
-        />
-      </Head>
+function MainPage() {
+  return <Provider store={store}></Provider>;
+}
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Splitify</h1>
-
-        <p className={styles.description}>
-          Get started by editing{"ash test"}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  );
-};
-
-export default Home;
+export default MainPage;
